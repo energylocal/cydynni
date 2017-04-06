1ea3f5da8b05c30ff311d8ed9faf52f8dabaafdd
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-04-06 11:21:02 +0100

    household load at start

--- pages/client.php
+++ pages/client.php
@@ -135,13 +135,14 @@ if (!session) {
 
 hydro_load();
 community_load();
+household_load();
 resize();
 
 // cydynni.js
 cydynnistatus_update();
 setInterval(cydynnistatus_update,10000);
 
-setInterval(community_load,10000);
+setInterval(community_load,60000);
 
 // -----------------------------------------------------------------------
 // Navigation and page selection
