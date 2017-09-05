6fa93d671c634970fd12a34372ae15c02244e240
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-09-05 18:41:02 +0100

    improved comments

--- scripts-hub/scheduler-algorithm-test.php
+++ scripts-hub/scheduler-algorithm-test.php
@@ -22,6 +22,8 @@ if (!$interruptible)
     // ---------------------------------------------------------------------------------
     // Method 1: move fixed period of demand over probability function to find best time
     // ---------------------------------------------------------------------------------
+    
+    // For each hour in 24 hour profile
     for ($i=0; $i<$len; $i++) {
 
          // Visual output
@@ -29,12 +31,14 @@ if (!$interruptible)
          print $i." ";
          for ($v=0; $v<($probability[$i]*10); $v++) print "|";
 
+         // Calculate sum of probability function values for block of demand covering hours in period
          $sum = 0;
          for ($h=0; $h<$period; $h++) {
              if ($i+$h>=$len) break;
              $sum += $probability[$i+$h];
          }
          
+         // Determine the start_time which gives the maximum sum of available power
          if ($sum>$max) {
              $max = $sum;
              $start_hour = $i;
@@ -43,6 +47,7 @@ if (!$interruptible)
          print " $sum\n";
     }
     
+    // Send start_time and period to run for to the device to be controlled
     print "Schedule start: $start_hour\n";
 
 } else {
@@ -54,22 +59,27 @@ if (!$interruptible)
 
     // For each hour of demand
     for ($h=0; $h<$period; $h++) {
-        // Find time of most available power
+
         $max = 0;
         $pos = 0;
+        // for each hour in probability profile
         for ($i=0; $i<$len; $i++) {
-        
+            // Find the hour with the maximum amount of available power
+            // that has not yet been alloated to this load
             if ($allocated[$i]==0 && $probability[$i]>$max) {
                 $max = $probability[$i];
                 $pos = $i;
             }
         }
         
+        // Allocate hour with maximum amount of available power
         $allocated[$pos] = 1;
     }
     
+    // The output here is the $allocated array which is then passed on to the load as a run a these hours command
+    
+    // Visual output for testing
     for ($i=0; $i<$len; $i++) {
-         // Visual output
          if ($i<10) print "0";
          print $i." ";
          for ($v=0; $v<($probability[$i]*10); $v++) print "|";
