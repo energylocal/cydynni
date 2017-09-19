fc03c40c918cf3d00ec1f6321ea6a00ea87a462e
TrystanLea <glyn.hudson@openenergymonitor.org>
2017-09-19 12:08:11 +0100

    seperate out demandshaper into seperate module

--- cydynni-module/cydynni_controller.php
+++ cydynni-module/cydynni_controller.php
@@ -24,64 +24,7 @@ function cydynni_controller()
     $result = false;
     
     switch ($route->action)
-    {  
-        case "schedule":
-            if ($session["write"]) {
-                $route->format = "html";
-                $result =  view("Modules/cydynni/schedule.php", array());
-            }
-            break;
-        
-        case "schedule-submit":
-            if ($session["write"]) {
-                $route->format = "json";
-                
-                if (isset($_GET['schedule'])) {
-                    include "$homedir/cydynni/scripts-hub/scheduler.php";
-                    
-                    $schedule = json_decode($_GET['schedule']);
-                    
-                    if (!isset($schedule->device)) return array("content"=>"Missing device parameter in schedule object");
-                    if (!isset($schedule->end)) return array("content"=>"Missing end parameter in schedule object");
-                    if (!isset($schedule->period)) return array("content"=>"Missing period parameter in schedule object");
-                    if (!isset($schedule->interruptible)) return array("content"=>"Missing interruptible parameter in schedule object");
-                    if (!isset($schedule->runonce)) return array("content"=>"Missing runonce parameter in schedule object");
-                    
-                    if ($schedule->runonce) $schedule->runonce = time();
-                    
-                    $result = schedule($schedule);
-                    
-                    $schedule->periods = $result["periods"];
-                    $schedule->probability = $result["probability"];
-                    
-                    $device = $schedule->device;
-                    
-                    $schedules = json_decode($redis->get("schedules"));
-                    if (!$schedules) $schedules = array();
-                    $schedules->$device = $schedule;
-                    $redis->set("schedules",json_encode($schedules));
-                    
-                    $result = array("schedule"=>$schedule);
-                } else {
-                    $result = "Schedule object not present";
-                }
-            }
-        
-            break;
-            
-        case "schedule-get":
-            if ($session["write"]) {
-                $route->format = "json";
-                if (isset($_GET['device'])) {
-                    $schedules = json_decode($redis->get("schedules"));
-                    $device = $_GET['device'];
-                    if (isset($schedules->$device)) $schedule = $schedules->$device;
-                    else $schedule = array();
-                    $result = array("schedule"=>$schedule);
-                }
-            }
-            break;
-    
+    {
         case "live":
             $route->format = "json";
             
