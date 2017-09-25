1678c77621c9300a47d7c9325c887ef07274cf11
trystanlea <trystan.lea@gmail.com>
2017-09-25 16:15:55 +0100

    tips section additions

--- cydynni_app_v2/client.php
+++ cydynni_app_v2/client.php
@@ -397,12 +397,98 @@ $v = 3;
 <!------------------------------------------------------------------------------------------------------------------->
 <!------------------------------------------------------------------------------------------------------------------->
 <!------------------------------------------------------------------------------------------------------------------->
+<style> /* temporary - needs to be moved */
+
+.tips-appliance img {
+    height: 250px;
+}
+
+.tips-appliance {
+    padding: 30px;
+}
+
+.tips-appliance figcaption {
+    display: block;
+    margin: auto;
+    width: 400px;
+}
+
+</style>
 
         <div class="page" name="tips">
             <div class="block">
                 <div class="block-title bg-tips"><?php echo t("Tips"); ?><div class="triangle-dropdown hide"></div></div>
                 <div class="block-content bg-tips" style="padding:20px">
-                    <img class="logo-full" src="images/dishwasher.png" style="height:200px;">
+                    <figure class="tips-appliance">
+                        <img src="images/dishwasher.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("DISHWASHER") ?></h2>
+                            </div>
+                            <?php echo t("The time you run your dishwasher can be moved to avoid morning and evening peaks and take advantage of hydro power and the cheaper prices in the daytime (11am - 4pm) and overnight (8pm - 6am).")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/lamp.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("LED LIGHTS") ?></h2>
+                            </div>
+                            <?php echo t("Switching off lights and appliance when not in use is a simple and effective way to use less electricity. You can make a special effort to do this during the morning and evening peaks.")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/stove.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("COOKING") ?></h2>
+                            </div>
+                            <?php echo t("Putting a lid on your pan when you're cooking traps the heat inside so you don’t need to have the hob on as high. A simple and effective way to use less electricity.")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/slowcooker.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("SLOW COOKING") ?></h2>
+                            </div>
+                            <?php echo t("Slow cookers are very energy efficient, make tasty dinners and help you avoid using electricity during the evening peak (4 - 8pm) when you might otherwise be using an electric oven.")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/washingmachine.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("WASHING MACHINE") ?></h2>
+                            </div>
+                            <?php echo t("The time you run your washing machine can be moved to avoid morning and evening peaks and take advantage of hydro power and the cheaper prices in the daytime (11am - 4pm) and overnight (8pm - 6am).")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/fridge.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("FRIDGES & FREEZERS") ?></h2>
+                            </div>
+                            <?php echo t("Try to minimise how often and how long you need to open the doors. Wait for cooked food to cool before putting it in the fridge. Older fridges and freezers can be very inefficient and costly to run.")
+                            ?>
+                        </figcaption>
+                    </figure>
+                    <figure class="tips-appliance">
+                        <img src="images/lightbulb.png">
+                        <figcaption>
+                            <div class="tips-appliance-name">
+                                <h2><?php echo t("LIGHTS") ?></h2>
+                            </div>
+                            <?php echo t("LED lights can cut your lighting costs by up to 90%. There’s more information on our website and in the info pack on installing them in your house.")
+                            ?>
+                        <figcaption>
+                    </figure>
                 </div>
             </div>
         </div>
