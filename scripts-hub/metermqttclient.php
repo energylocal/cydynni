2b41cece592669e8733e5b6bb46534e01b7704ad
Glyn <glyn.hudson@gmail.com>
2018-01-22 13:21:39 +0000

    process frame and save to redis

--- scripts-hub/metermqttclient.php
+++ scripts-hub/metermqttclient.php
@@ -3,6 +3,13 @@
 // Load MQTT server settings
 require "/home/pi/cydynni/scripts-hub/settings.php";
 
+$redis = new Redis();
+if (!$redis->connect($redis_server['host'], $redis_server['port'])) die;
+if (!empty($redis_server['prefix'])) $redis->setOption(Redis::OPT_PREFIX, $redis_server['prefix']);
+if (!empty($redis_server['auth'])) {
+    if (!$redis->auth($redis_server['auth'])) die;
+}
+
 $mqtt_client = new Mosquitto\Client();
 
 $connected = false;
@@ -77,5 +84,30 @@ function message($message)
     $topic = $message->topic;
     $value = $message->payload;
     print $topic." ".$value."\n";
-        
+    process_frame($topic,$value);
+}
+
+function process_frame($topic,$value) 
+{
+    global $redis,$meter_topic;
+    
+    if ($topic==$meter_topic) {
+        /* Example data: {"cumulative":{
+            "serial":"----------",
+            "timeStamp":"2018-01-17T05:06:10.000Z",
+            "import_kWh":1326.962,
+            "export_kWh":0,
+            "import_kvarh":18.666,
+            "export_kvarh":137.601,
+            "import_kVAh":1312.873,
+            "export_kVAh":0
+        }} */
+      
+        $data = json_decode($value);
+        if ($data!=null && is_object($data)) {
+            if (isset($data->cumulative)) {
+                $redis->set($meter_topic,$value);
+            }
+        }
+    }
 }
