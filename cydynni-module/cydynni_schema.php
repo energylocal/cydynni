fa9bd7c998aa40a00a84d56a2e97300a4ac4ecc3
Emrys <emrys@openenergymonitor.org>
2018-05-10 13:06:05 +0100

    modified schema to include clubs

--- cydynni-module/cydynni_schema.php
+++ cydynni-module/cydynni_schema.php
@@ -6,5 +6,16 @@ $schema['cydynni'] = array(
     'token' => array('type' => 'varchar(128)'),
     'premisestoken' => array('type' => 'varchar(128)'),
     'welcomedate' => array('type' => 'varchar(32)', "default"=>"not sent"),
-    'reportdate' => array('type' => 'varchar(32)', "default"=>"not sent")
+    'reportdate' => array('type' => 'varchar(32)', "default"=>"not sent"),
+    'clubs_id' => array('type'=>'int(11)')
+);
+$schema['cydynni_clubs'] = array(
+    'name' => array('type' => 'varchar(128)'),
+    'generator' => array('type' => 'varchar(128)'),
+    'root_token' => array('type' => 'varchar(128)'),
+    'api_prefix' => array('type' => 'int(11)'),
+    'languages' => array('type' => 'text'),
+    'generation_feed' => array('type' => 'int(11)'),
+    'consumption_feed' => array('type' => 'int(11)'),
+    'color' => array('type' => 'varchar(11)')
 );
