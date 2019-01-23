be7a9290440052a65d6665b210aa5462205a2b63
TrystanLea <trystan.lea@gmail.com>
2019-01-23 13:17:40 +0000

    fix restart procedure

--- scripts-hub/meterread/meterread.php
+++ scripts-hub/meterread/meterread.php
@@ -6,38 +6,6 @@ require "process_settings.php";
 require "core.php";
 
 // ----------------------------------------------------------
-// 1. Find smartmeter ip address
-// ----------------------------------------------------------
-print "Scanning for smartmeter\n";
-exec("nmap -sP 192.168.0.1/24 | grep 'Nmap scan report for' | cut -d' ' -f5",$output);
-print "Found ".count($output)." devices\n";
-$meter_ip = false;
-foreach ($output as $ip) {
-    
-    $ip_parts = explode(".",$ip);
-    if (count($ip_parts)==4) {
-    
-        $ip1 = (int) $ip_parts[0];
-        $ip2 = (int) $ip_parts[1];
-        $ip3 = (int) $ip_parts[2]; 
-        $ip4 = (int) $ip_parts[3]; 
-    
-        // print "$ip1.$ip2.$ip3.$ip4\n";
-        $result = exec("curl --silent --max-time 2.0 http://$ip1.$ip2.$ip3.$ip4/ajax/meterread");
-        $json = json_decode($result);
-        if ($json!=null) {
-            $meter_ip = "$ip1.$ip2.$ip3.$ip4";
-        }
-    }
-}
-
-if ($meter_ip==false) {
-    print "smartmeter not found\n";
-    die;
-}
-print "smartmeter ip: ".$meter_ip."\n";
-
-// ----------------------------------------------------------
 // 2. Fetch apikey write from emoncms mysql database
 // ----------------------------------------------------------
 $mysqli = @new mysqli($server,$username,$password,$database,$port);
@@ -60,21 +28,69 @@ print http_request("GET","http://localhost/emoncms/device/autocreate.json",array
 // 4. Poll for data
 // ----------------------------------------------------------
 $interval = 10;
-$last = 0;
-while(true) {
+$last = time()-10;
+$ip_found = false;
+$last_valid = 0;
 
+while(true) {
     $now = time();
+    
     if (($now-$last)>=$interval) {
         $last = $now;
+        
+        if (!$ip_found || (time()-$last_valid)>60) {
+            $ip_scan_result = ip_scan();
+            if ($ip_scan_result!=false) {
+                $ip_found = true;
+                $meter_ip = $ip_scan_result;
+            }
+            $last_valid = time();
+        }
+        
+        if ($ip_found) {
+            $result = http_request("GET",$meter_ip."/ajax/meterread",array());
+            $json = json_decode($result);
 
-        $result = http_request("GET",$meter_ip."/ajax/meterread",array());
-        $json = json_decode($result);
-
-        if ($json!=null) {
-            foreach ($json as $key=>$val) $json->$key = 1*$val;
-            if (isset($json->time)) unset($json->time);
-            print http_request("POST","http://localhost/emoncms/input/post",array("node"=>"smartmeter","fulljson"=>json_encode($json),"apikey"=>$apikey))."\n";
+            if ($json!=null) {
+                $last_valid = time();
+                foreach ($json as $key=>$val) $json->$key = 1*$val;
+                if (isset($json->time)) unset($json->time);
+                print http_request("POST","http://localhost/emoncms/input/post",array("node"=>"smartmeter","fulljson"=>json_encode($json),"apikey"=>$apikey))."\n";
+            } else {
+                print "no response\n";
+            }
         }
     }
     sleep(1);
+}
+
+function ip_scan()
+{
+    print "Scanning for smartmeter\n";
+    exec("nmap -sP 192.168.0.1/24 | grep 'Nmap scan report for' | cut -d' ' -f5",$output);
+    print "Found ".count($output)." devices\n";
+    $meter_ip = false;
+    foreach ($output as $ip) {
+        
+        $ip_parts = explode(".",$ip);
+        if (count($ip_parts)==4) {
+        
+            $ip1 = (int) $ip_parts[0];
+            $ip2 = (int) $ip_parts[1];
+            $ip3 = (int) $ip_parts[2]; 
+            $ip4 = (int) $ip_parts[3]; 
+        
+            // print "$ip1.$ip2.$ip3.$ip4\n";
+            $result = exec("curl --silent --max-time 2.0 http://$ip1.$ip2.$ip3.$ip4/ajax/meterread");
+            $json = json_decode($result);
+            if ($json!=null) {
+                $meter_ip = "$ip1.$ip2.$ip3.$ip4";
+            }
+        }
+    }
+
+    if ($meter_ip!=false) {
+        print "smartmeter ip: ".$meter_ip."\n";
+    }
+    return $meter_ip;
 }
