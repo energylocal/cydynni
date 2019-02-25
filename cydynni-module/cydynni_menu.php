00283397292c7ae4692397b8aa8c7938181581f0
TrystanLea <trystan.lea@gmail.com>
2019-02-25 12:55:45 +0000

    menu cymraeg

--- cydynni-module/cydynni_menu.php
+++ cydynni-module/cydynni_menu.php
@@ -1,6 +1,6 @@
 <?php
 
-    global $lang;
+    global $lang, $session;
 
     $domain = "messages";
     
@@ -9,19 +9,21 @@
 
     if (isset($_GET['lang'])) $apikeystr .= "&lang=".$_GET['lang'];
     
+    $name = "Dashboard"; if ($session["lang"]=="cy_GB") $name = "Dashfwrdd";
     $menu_left[] = array(
         'id'=>"cydynni_menu",
-        'name'=>"Dashboard",
+        'name'=>$name,
         'path'=>"".$apikeystr , 
         'session'=>"read", 
         'order' => 3,
         'icon'=>'icon-th-large icon-white',
         'hideinactive'=>0
     );
-    
+
+    $name = "Report"; if ($session["lang"]=="cy_GB") $name = "Adroddiad";
     $menu_left[] = array(
         'id'=>"cydynni_report_menu",
-        'name'=>"Reports",
+        'name'=>$name,
         'path'=>"cydynni/report".$apikeystr , 
         'session'=>"read", 
         'order' => 4,
