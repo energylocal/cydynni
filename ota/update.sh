dcb915a03c454020f14f414e67d594538213f9af
Trystan Lea <trystan.lea@googlemail.com>
2019-03-06 10:34:09 +0000

    Update update.sh

--- ota/update.sh
+++ ota/update.sh
@@ -7,22 +7,21 @@ cd
 apikey="$(php /home/pi/cydynni/ota/getapikey.php)"
 local_ota_version="$(cat cydynni/ota/version)"
 
-url="https://emoncms.cydynni.org.uk/cydynni/ota-version?hub=$local_ota_version&apikey=$apikey"
+url="https://dashboard.energylocal.org.uk/cydynni/ota-version?hub=$local_ota_version&apikey=$apikey"
 remote_ota_version="$(curl -s $url)"
 
 if [ "$remote_ota_version" -ne "$local_ota_version" ]
 then
   echo "Remote:"$remote_ota_version" Local:"$local_ota_version
   echo "Update available, starting update process.."
-  rpi-rw
-  cd cydynni
-  git pull origin master
-  cd ota
-  log=$(./main.sh)
-  cd
-  rpi-ro
+  # rpi-rw
+  cd /home/pi/cydynni
+  git pull
+  log=$(./home/pi/cydynni/ota/main.sh)
+  cd 
+  # rpi-ro
 
   echo $"$log"
-  url="https://emoncms.cydynni.org.uk/cydynni/ota-log-set?apikey=$apikey"
+  url="https://dashboard.energylocal.org.uk/cydynni/ota-log-set?apikey=$apikey"
   $(curl -s -X POST -d "$log" $url)
 fi
