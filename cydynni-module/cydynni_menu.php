72e7eada6d28c13172239aff7f4fdf6f2f5e45f3
Emrys <emrys@openenergymonitor.org>
2019-06-12 11:17:45 +0100

    added dark sidebar

--- cydynni-module/cydynni_menu.php
+++ cydynni-module/cydynni_menu.php
@@ -1,33 +1,25 @@
 <?php
 
-    global $lang, $session;
-
-    $domain = "messages";
-    
     $apikeystr = "";
     if (isset($_GET['apikey'])) $apikeystr = "?apikey=".$_GET['apikey'];
-
     if (isset($_GET['lang'])) $apikeystr .= "&lang=".$_GET['lang'];
     
-    $name = "Dashboard"; if ($session["lang"]=="cy_GB") $name = "Dashfwrdd";
-    $menu_left[] = array(
-        'id'=>"cydynni_menu",
-        'name'=>$name,
-        'path'=>"".$apikeystr , 
-        'session'=>"read", 
-        'order' => 3,
-        'icon'=>'icon-th-large icon-white',
-        'hideinactive'=>0
-    );
-
-    $name = "Report"; if ($session["lang"]=="cy_GB") $name = "Adroddiad";
-    $menu_left[] = array(
-        'id'=>"cydynni_report_menu",
-        'name'=>$name,
-        'path'=>"cydynni/report".$apikeystr , 
-        'session'=>"read", 
+    // navbar link
+    $menu['tabs'][] = array(
+        'icon'=>'cydynni',
+        'title'=> dgettext("dashboard_messages","Dashboards"),
+        'path'=> "cydynni".$apikeystr,
         'order' => 4,
-        'icon'=>'icon-book icon-white',
-        'hideinactive'=>0
+        'data'=> array('sidebar' => '#sidebar_cydynni')
     );
 
+    $menu['sidebar']['cydynni'][] = array(
+        'text' => $session["lang"]=="cy_GB" ? "Dashfwrdd" : "Dashboard",
+        'path'=> "cydynni".$apikeystr, 
+        'order' => 1
+    );
+    $menu['sidebar']['cydynni'][] = array(
+        'text' => $session["lang"]=="cy_GB" ? "Adroddiad" : "Report",
+        'path'=> "cydynni/report".$apikeystr, 
+        'order' => 2
+    );
