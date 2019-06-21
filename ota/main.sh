68308bae19d71a6fed94d4e6003e9ae952bb3929
Trystan Lea <trystan.lea@googlemail.com>
2019-06-21 13:00:20 +0100

    Update main.sh

--- ota/main.sh
+++ ota/main.sh
@@ -4,6 +4,10 @@ cd $DIR
 
 echo "-----------------------------------------"
 
+/home/pi/emonpi/service-runner-update.sh
+
+echo "-----------------------------------------"
+
 cd /home/pi/cydynni
 branch="$(git rev-parse --abbrev-ref HEAD)"
 commit="$(git rev-parse HEAD)"
