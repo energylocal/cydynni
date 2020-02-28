7b58cdafb087a5f975cc52c4459a2f6b00bc8890
= <=>
2020-02-28 09:58:45 +0000

    corrected translation placeholder again

--- cydynni-module/app/client_forecast_view.php
+++ cydynni-module/app/client_forecast_view.php
@@ -153,7 +153,7 @@
                         <tr>
                         <th scope="row">
                             <span class="text-day d-sm-inline d-lg-none"><?php echo t("Day") ?></span>
-                            <span class="text-day d-none d-md-inline d-lg-inline"> <?php echo t("Day Price") ?></span> 
+                            <span class="text-day d-none d-md-inline d-lg-inline"> <?php echo t("Midday Price") ?></span> 
                             <br class="d-sm-none">
                             <span class="font-weight-light text-smaller-sm">7<?php echo t("am") ?> - 4<?php echo t("pm") ?></span></th>
                             <td>10.4<?php echo t("p") ?> <span class="font-weight-light d-none d-sm-inline">(-45%)</span></td>
