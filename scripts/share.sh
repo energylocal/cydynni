f12b47b7288f08fa2216c9a6c8f8b022e60deda1
TrystanLea <trystan.lea@gmail.com>
2021-11-10 11:03:12 +0000

    remove sync method to pull repower data

--- scripts/share.sh
+++ scripts/share.sh
@@ -2,10 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $DIR
 
-sync_str=$(cat $DIR/sync_string.json)
-redis-cli rpush sync-queue $sync_str
+# sync_str=$(cat $DIR/sync_string.json)
+# redis-cli rpush sync-queue $sync_str
+# php /opt/emoncms/modules/sync/sync_run.php
 
-php /opt/emoncms/modules/sync/sync_run.php
+php $DIR/get_repower_solar.php
 
 php $DIR/process_power_feeds_run.php
 php $DIR/mergefeeds_run.php
