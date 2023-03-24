3abfd1cf977c32121fbf5bddd873568f423d2041
TrystanLea <trystan.lea@gmail.com>
2023-03-24 17:12:22 +0000

    config file

---
+++ scripts/config.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+CLUB IDS
+1    Bethesda
+2    Repower
+3    Tower Power
+4    Corwen
+5    Crickhowell
+6    Machynlleth
+7    Bridport
+8    Llandysul
+9    Milfordhaven
+10   Test
+11   Roupellpark
+12   Capel dewi
+13   Dyffryn banw
+
+*/
+
+$recalc_club = false;
+$recalc_all = false;
+$recalc_period = 3600*24*7;
+$userid = false;
+
+
+
