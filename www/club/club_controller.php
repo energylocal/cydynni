42fe157cac06aa5ab8f6475df76d22c19b79c9f7
photekq <photekq@gmail.com>
2025-07-02 14:16:03 +0100

    Moved functions to symfony from swiftmailer

--- www/club/club_controller.php
+++ www/club/club_controller.php
@@ -489,7 +489,7 @@ function club_controller()
     // Admin register email
     if ($route->action == "admin-registeremail" && $session['admin']) {
         $route->format = "text";
-        require("Lib/email.php");
+        require("Lib/symfony_email.php");
         require("Modules/club/club_emails.php");
         $club_emails = new ClubEmails($mysqli);
         return $club_emails->registeremail(get('userid'));
@@ -511,7 +511,7 @@ function club_controller()
     // Admin send report
     if ($route->action == "admin-sendreport" && $session['admin']) {
         $route->format = "text";
-        require("Lib/email.php");
+        require("Lib/symfony_email.php");
         require("Modules/club/club_emails.php");
         $club_emails = new ClubEmails($mysqli);
         return $club_emails->send_report_email(get('userid'));
